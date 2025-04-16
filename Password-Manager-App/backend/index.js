d58835c15c03796447de60803c5ed2aa1215cad4
Briana Daniels <briana.daniels@hp.com>
2025-04-16 15:52:59 -0500

    Still working on the API endpoints to ensure that are displaying

--- Password-Manager-App/backend/index.js
+++ Password-Manager-App/backend/index.js
@@ -1,5 +1,13 @@
 const {db} = require('./database/connection.js');
 const PassVault = require('./model/PassVault.js');
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 
 (async () => {
     try {
@@ -50,6 +58,64 @@ try {
     console.error('Error adding entries to PassVault:', error);
 }
 })();
+
+
+app.get('/passwords', async (req, res) => {
+    try {
+        const entries = await PassVault.findAll({attributes: ['password']});
+        res.json(entries);
+    } catch (error) {
+        console.error('Error fetching entries:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+app.get('/usernames', async (req, res) => {
+    try {
+        const entries = await PassVault.findAll({ attributes: ['username'] });
+        res.json(entries);
+    } catch (error) {
+        console.error('Error fetching usernames:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+app.get('/notes', async (req, res) => {
+    try {
+        const entries = await PassVault.findAll({ attributes: ['notes'] });
+        res.json(entries);
+    } catch (error) {
+        console.error('Error fetching notes:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.get('password/search', async (req, res) => {
+const { website } = req.query;
+try {
+    const entry = await PassVault.findOne({ where: { website } });
+    if (entry){
+        res.json(entry);
+    } else {
+        res.status(404).json({ error: 'Does not exist' });
+    } 
+} catch (error) {
+        console.error('Error searching for password:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+
+
+const PORT = 3000;;
+app.listen(PORT, async () => {
+    try {
+        await db.sync();
+        console.log(`Server is running on http://localhost:${PORT}`);
+    } catch (error) {
+        console.error('Error syncing database:', error);
+    }
+});
+
 // Export the database connection for use in other modules
 module.exports = {
     db,
