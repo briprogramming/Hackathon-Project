91924a6935cd8e9b12209b819e84ea99cb46f634
Briana Daniels <briana.daniels@hp.com>
2025-04-22 11:28:32 -0500

    Corrections on add frontend

--- Password-Manager-App/backend/index.js
+++ Password-Manager-App/backend/index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend')));
-app.get('*', (req, res) => {
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
